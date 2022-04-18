9463a1f5772e969bc7e1b497a7606281912de93f
Andrei Evandro Karas <69061189+andreikaras@users.noreply.github.com>
2022-04-18 10:40:11 -0300

    Idade em Dias

---
+++ C/1. Iniciante/1020.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    scanf("%d", &n);
+
+    printf("%d ano(s)\n", n/365);
+    n %= 365;
+    printf("%d mes(es)\n", n/30);
+    n %= 30;
+    printf("%d dia(s)\n", n);
+    return 0;
+}
