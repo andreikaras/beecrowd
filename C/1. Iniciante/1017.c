30dc8c8368ea304e90a2646060eb055bbc93fa68
Andrei Evandro Karas <69061189+andreikaras@users.noreply.github.com>
2022-04-18 09:19:32 -0300

    Gasto de Combustível

---
+++ C/1. Iniciante/1017.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define REND 12
+
+int main(void)
+{
+    int tempo, velMedia;
+
+    scanf("%d%d", &tempo, &velMedia);
+
+    printf("%.3f\n", (float) tempo * velMedia / REND);
+    return 0;
+}
